40ed77ec01a7fd81936746d3c6cf3d9773a9e2cb
ramsha275 <33019590+ramsha275@users.noreply.github.com>
2020-05-15 21:47:11 +0500

    Filter numbers with a list comprehension

---
+++ Day 7/bite 107.py
@@ -0,0 +1,8 @@
+def filter_positive_even_numbers(numbers):
+    """Receives a list of numbers, and returns a filtered list of only the
+       numbers that are both positive and even (divisible by 2), try to use a
+       list comprehension."""
+    even = [num for num in numbers if num%2==0 and num>0]
+    return even
+    
+
