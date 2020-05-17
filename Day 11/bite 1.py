6a8c03951b8b39d85fd5beab3aacd8815077a089
ramsha275 <33019590+ramsha275@users.noreply.github.com>
2020-05-18 02:20:59 +0500

    Sum n numbers

---
+++ Day 11/bite 1.py
@@ -0,0 +1,23 @@
+def sum_numbers(numbers=None):
+    
+    ''' Write a function that can sum up numbers:
+
+        It should receive a list of n numbers.
+        If no argument is provided, return sum of numbers 1..100.
+        Look closely to the type of the function's default argument ...'''
+    
+    if numbers == None :
+        return sum(range(1,101))
+    else:
+        return sum(numbers)
+
+
+    #OR
+
+
+def sum_numbers(numbers=None):
+
+    if numbers is None :
+        numbers = range(1,101)
+   
+    return sum(numbers)
